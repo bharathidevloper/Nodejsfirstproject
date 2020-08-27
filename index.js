d0e0305a599e54acb1dc8904493a0f19b1e4725f
bharathidevloper <bharuuma12@gmail.com>
2020-08-27 12:59:59 +0530

    seeting for hosting

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const login_route = require('./routefolder/login');
 
-const port = 3000;
-app.listen(port, console.log("Server is running on 3000 port"));
+const port = process.env.PORT || 8080;
+app.listen(port, console.log("Server is running on 8080 port"));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use('/api', login_route);
 
